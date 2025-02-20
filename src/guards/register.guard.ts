036f62a1ae438ba7949af171131975d5b43c503b
arthur pereira <artwozera@gmail.com>
2025-02-20 18:36:58 -0300

    feat: add register guard

---
+++ src/guards/register.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { LoggerService } from 'src/modules/logger/logger.service';
+
+@Injectable()
+export class ApiTokenGuard implements CanActivate {
+    constructor(private readonly logger: LoggerService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    this.logger.log('ApiTokenGuard Check');
+    const request = context.switchToHttp().getRequest();
+    const authorization = request.headers.authorization;
+
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+
+    const token = authorization.split(' ')[1];
+    const apiToken = process.env.API_TOKEN;
+
+    if (token !== apiToken) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+    this.logger.log('ApiTokenGuard Check Passed');
+    return true;
+  }
+}
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,14 +5,17 @@ import {
   Headers,
   Post,
   UnauthorizedException,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiTokenGuard } from 'src/guards/register.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(ApiTokenGuard)
   @Post('signup')
   async signUp(@Body() createUserDto: CreateUserDto) {
     return this.authService.signUp(createUserDto);
